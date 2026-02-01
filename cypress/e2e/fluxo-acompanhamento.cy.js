a08558c6f8043674a506f6d6c149af53f7e8989b
Paulo-H-P <120729175+Paulo-H-P@users.noreply.github.com>
2026-02-01 11:59:06 -0300

    fix: corrige imports do fluxo-acompanhamento.cy.js

--- cypress/e2e/fluxo-acompanhamento.cy.js
+++ cypress/e2e/fluxo-acompanhamento.cy.js
@@ -1,4 +1,4 @@
-import HomeCadastrarPage from '../Pages/Home_cadastrar'
+import HomeCadastrarPage from './Pages/home_cadastrar'
 import CadastrarPage from './Pages/cadastrar'
 
 describe('Home → Cadastro', () => {
